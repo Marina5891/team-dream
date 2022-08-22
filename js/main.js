dfdc5c40bc24bbd984e8e9bd0a8edbf469cccfad
Marina5891 <marinachist1985@gmail.com>
2022-08-22 16:28:31 +0500

    Add js-style

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+  /* Плавная прокрутка меню */
+
+  $('nav a[href^="#"]').not('.hide').click(function () {
+    let target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 500);
+    $('nav a[href^="#"]').parent().removeClass('active');
+    $(this).parent().addClass('active');
+    $('.menu__mobile .menu').toggle(500);
+    $('.menu__burger').toggleClass('close');
+    return false;
+  });
+
+  /* Плавная прокрутка меню для Home & Logo */
+
+  $('nav a.hide').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+
+    $('nav a[href^="#"]').parent().removeClass('active');
+    $(this).parent().addClass('active');
+    return false;
+  });
+
+  $('.menu__burger').click(function () {
+    $('.menu__mobile .menu').toggle(500);
+    $(this).toggleClass('close');
+  });
+
+});
